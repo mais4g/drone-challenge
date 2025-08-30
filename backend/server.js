3add1a795ddf99ce186f289e84fba9ab218932d6
Maisa Kichler Gomes <maisakichlergms@gmail.com>
2025-08-30 17:06:44 -0300

    up

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const fastify = require("fastify")({ logger: true });
-const cors = require("@fastify/cors");
+//const cors = require("@fastify/cors");
 
+/*
 fastify.register(cors, {
   origin: "http://localhost:5173", 
   methods: ["GET", "POST", "PATCH", "DELETE"], 
 });
+*/
 
 const pedidoRoutes = require("./routes/pedidoRoutes");
 const droneRoutes = require("./routes/droneRoutes");
@@ -18,7 +20,7 @@ fastify.register(entregaRoutes, { prefix: '/api' });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    await fastify.listen({ port: 3000 });
     fastify.log.info("🚀 Servidor rodando em http://localhost:3000");
   } catch (err) {
     fastify.log.error(err);
